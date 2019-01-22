beb5c2977fd304688cc047e98151a09f7e578278
Phuong261217 <phuonglpde130117@fpt.edu.vn>
2019-01-22 10:46:28 +0700

    Update

--- Code/src/control/Grass.java
+++ Code/src/control/Grass.java
@@ -3,33 +3,46 @@ package control;
 import java.util.Random;
 
 public class Grass {
-	
 	public int nColumns;
 	public int nRows;
 	private double [][] cells;
 	private Random rand;
-	
+	private static double MAX_GROWING_SPEED = 0.1;
+
 	public Grass(int nColumns, int nRows) {
 		super();
-		Random rand = new Random();
 		this.rand = new Random();
 		this.nColumns = nColumns;
 		this.nRows = nRows;
 		this.cells = new double [nRows][nColumns];
-		for(int i=0;i<nRows;i++) {
-			for(int j=0;j<nColumns;j++) {	
-				
-				cells[i][j]=rand.nextFloat();
+		for (int i=0;i<nRows;i++) {
+			for(int j=0;j<nColumns;j++) 
+			{
+				cells[i][j] = rand.nextDouble();
 				
 			}
 		}
 	}
-	public double getCell(int i, int j) {
+	public double getCell (int i, int j) {
 		return cells[i][j];
 	}
+	public void grow() {
+		
+		Random rand = new Random();
+		for (int i=0;i<nRows;i++) {
+			for(int j=0;j<nColumns;j++) 
+			{
+				cells[i][j] += rand.nextDouble()*MAX_GROWING_SPEED;
+				//if(cells[i][j]>1) cells[i][j] =1;
+				cells[i][j]=(cells[i][j]>1)?1:cells[i][j];
+			}
+		}	
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//kscnksn
+
 	}
 
 }
--- Code/src/view/GrassConsole.java
+++ Code/src/view/GrassConsole.java
@@ -1,17 +1,27 @@
 package view;
-
 import control.Grass;
-public class GrassConsole {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Grass grass = new Grass(5,7);
-		for(int i=0; i < grass.nRows; i ++) {
-			for(int j=0; j< grass.nColumns; j++) {
+public class GrassConsole {
+	private static void display(Grass grass) {
+		for (int i=0; i< grass.nRows;i++)
+		{
+			for (int j=0; j< grass.nColumns; j++)
+			{
 				//System.out.println(grass.getCell(i, j));
-				System.out.print( String.format( "%.2f", grass.getCell(i, j) ) + " ");
+				System.out.print( String.format( "%.2f", grass.getCell(i, j) ) + " " );
 			}
 			System.out.println();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Grass grass = new Grass(5,7);
+		display(grass);
+		for (int i=0;i<5;i++) {
+			grass.grow();
+			System.out.println("//////////////////////");
+			display(grass);
 		}
 	}
 
